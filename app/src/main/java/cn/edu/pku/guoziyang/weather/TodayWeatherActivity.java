462e1fe83afd549b0617325942cc23934dc78a1c
guoziyang <guoziyang@yeah.net>
2016-12-10 13:47:42 +0800

    添加"检查网络状态"功能

--- app/src/main/java/cn/edu/pku/guoziyang/weather/TodayWeatherActivity.java
+++ app/src/main/java/cn/edu/pku/guoziyang/weather/TodayWeatherActivity.java
@@ -3,11 +3,14 @@ package cn.edu.pku.guoziyang.weather;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import cn.edu.pku.guoziyang.util.NetUtil;
 
 /**
  * 今日天气信息界面
@@ -16,7 +19,7 @@ import android.widget.Toast;
 public class TodayWeatherActivity extends Activity {
 
     //Log信息标签
-    private String TAG = "mini-weather";
+    private String TAG = "myWeather";
 
 
     //按钮
@@ -30,6 +33,10 @@ public class TodayWeatherActivity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_today);//关联布局
+
+
+        //检测是否连接到网络
+        checkNetworkState();
 
         //选择城市按钮
         mCitySelect = (ImageView) findViewById(R.id.title_city_manager);
@@ -52,5 +59,18 @@ public class TodayWeatherActivity extends Activity {
             }
         });
         //初始化控件
+    }
+
+
+    //检测是否连接到网络
+    protected void checkNetworkState(){
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+            Log.d(TAG, "网络OK" );
+            Toast.makeText(TodayWeatherActivity.this, "网络OK!", Toast.LENGTH_LONG).show();
+            //更新天气信息，不需要提示网络连接成功
+        } else {
+            Log.d(TAG, "网络挂了");
+            Toast.makeText(TodayWeatherActivity.this, "网络连接失败，请检查网络!", Toast.LENGTH_LONG).show();
+        }
     }
 }
